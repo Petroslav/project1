1ebc98f1f9950be8ffa54b6e37267163c626b7e8
Pepi <petroslav.angelov@gmail.com>
2018-07-26 19:07:39 +0300

    updated DB user and added a question for Pesho

--- src/main/java/kekproject/kekproject2/Kekproject2Application.java
+++ src/main/java/kekproject/kekproject2/Kekproject2Application.java
@@ -9,6 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Kekproject2Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Kekproject2Application.class, args);
+
+
+    //TODO питай пешо дали тези индекси реално са форен кий блят
     }
+
 }
